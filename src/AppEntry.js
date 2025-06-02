995491103ea61d5279fd02189896f30c1055dedb
CD Cabrera <cabrera.code@gmail.com>
2025-06-02 16:52:48 -0400

    DEV one step closer

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux';
 import App from './app';
-// import './styles/index.scss';
+import './styles/index.scss';
 // import '@patternfly/react-styles/css/components/Select/select.css';
 
 /**
