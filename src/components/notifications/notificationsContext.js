89decdd70c11938956c4addb9fbaf1d1faa22e16
CD Cabrera <cabrera.code@gmail.com>
2025-06-16 10:30:09 -0400

    DEV stash

---
+++ src/components/notifications/notificationsContext.js
@@ -0,0 +1,9 @@
+import { useNotifications } from '@redhat-cloud-services/frontend-components-notifications';
+
+// const useNotifications = () => usePlatformNotifications();
+
+const context = {
+  useNotifications
+};
+
+export { context as default, context, useNotifications };
